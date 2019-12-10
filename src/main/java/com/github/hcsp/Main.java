8aaf61ae60ac79d0911d34dbb554173a6d3f1464
ws1603 <18388866683@163.com>
2019-12-10 19:01:48 +0800

    修改配置

---
+++ src/main/java/com/github/hcsp/Main.java
@@ -0,0 +1,30 @@
+package com.github.hcsp;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://sina.cn/?vt=4");
+
+        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            EntityUtils.consume(entity1);
+
+            System.out.println(EntityUtils.toString(entity1));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
